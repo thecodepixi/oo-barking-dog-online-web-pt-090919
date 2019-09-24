a7160863eda77fbcdbd0280193c9c99bed7a1fe2
Emily Harber <emily.a.harber@gmail.com>
2019-09-24 02:01:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog 
+  def name=(my_dog_name)
+    @dog_name = my_dog_name
+  end 
+  
+  def name 
+    @dog_name 
+  end 
+  
+  def bark 
+    puts "woof!"
+  end 
+end 
